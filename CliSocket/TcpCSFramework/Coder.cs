50122275d49bd77d7ab7564ffb1c4cb491d28a0d
dodong471520 <1012131075@qq.com>
2014-02-26 11:43:25 +0800

    work

---
+++ CliSocket/TcpCSFramework/Coder.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TcpCSFramework
+{
+    /// <summary>
+    /// 通讯服务提供编码和解码服务.
+    /// </summary>
+    public class Coder
+    {
+        /// <summary>
+        /// 编码方式
+        /// </summary>
+        private EncodingMothord _encodingMothord;
+
+        protected Coder()
+        {
+
+        }
+
+        public Coder(EncodingMothord encodingMothord)
+        {
+            _encodingMothord = encodingMothord;
+        }
+
+        public enum EncodingMothord
+        {
+            Default = 0,
+            Unicode,
+            UTF8,
+            ASCII,
+        }
+
+        /// <summary>
+        /// 通讯数据解码
+        /// </summary>
+        /// <param name="dataBytes">需要解码的数据</param>
+        /// <returns>编码后的数据</returns>
+        public virtual string GetEncodingString(byte[] dataBytes, int size)
+        {
+            switch (_encodingMothord)
+            {
+                case EncodingMothord.Default:
+                    {
+                        return Encoding.Default.GetString(dataBytes, 0, size);
+                    }
+                case EncodingMothord.Unicode:
+                    {
+                        return Encoding.Unicode.GetString(dataBytes, 0, size);
+                    }
+                case EncodingMothord.UTF8:
+                    {
+                        return Encoding.UTF8.GetString(dataBytes, 0, size);
+                    }
+                case EncodingMothord.ASCII:
+                    {
+                        return Encoding.ASCII.GetString(dataBytes, 0, size);
+                    }
+                default:
+                    {
+                        throw (new Exception("未定义的编码格式"));
+                    }
+            }
+
+        }
+
+        /// <summary>
+        /// 数据编码
+        /// </summary>
+        /// <param name="datagram">需要编码的报文</param>
+        /// <returns>编码后的数据</returns>
+        public virtual byte[] GetEncodingBytes(string datagram)
+        {
+            switch (_encodingMothord)
+            {
+                case EncodingMothord.Default:
+                    {
+                        return Encoding.Default.GetBytes(datagram);
+                    }
+                case EncodingMothord.Unicode:
+                    {
+                        return Encoding.Unicode.GetBytes(datagram);
+                    }
+                case EncodingMothord.UTF8:
+                    {
+                        return Encoding.UTF8.GetBytes(datagram);
+                    }
+                case EncodingMothord.ASCII:
+                    {
+                        return Encoding.ASCII.GetBytes(datagram);
+                    }
+                default:
+                    {
+                        throw (new Exception("未定义的编码格式"));
+                    }
+            }
+        }
+    }
+}
---
+++ CliSocket/TcpClient/TcpClient.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TcpCSFramework;
+
+namespace TcpClient
+{
+    /// <summary>
+    /// Client
+    /// </summary>
+    public class TcpClient
+    {
+        public TcpClient()
+        {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Begin to Test TcpCli Class..");
+            //ArrayList al = new ArrayList();
+
+            TcpClient test = new TcpClient();
+            TcpCli cli1 = new TcpCli(new Coder(Coder.EncodingMothord.Default));
+
+            cli1.Resovlver = new DatagramResolver("]}");
+            cli1.ReceivedDatagram += new NetEvent(test.RecvData);
+            cli1.DisConnectedServer += new NetEvent(test.ClientClose);
+            cli1.ConnectedServer += new NetEvent(test.ClientConn);
+
+            try
+            {
+                //命令控制循环
+                while (true)
+                {
+                    Console.Write(">");
+                    string cmd = Console.ReadLine();
+
+                    if (cmd.ToLower() == "exit")
+                    {
+                        break;
+                    }
+                    if (cmd.ToLower() == "close")
+                    {
+                        //cli.Close();
+                        continue;
+                    }
+
+                    if (cmd.ToLower().IndexOf("conn") != -1)
+                    {
+                        cmd = cmd.ToLower();
+                        string[] para = cmd.Split(' ');
+
+                        if (para.Length != 3)
+                        {
+                            Console.WriteLine("Error Command");
+                            continue;
+                        }
+
+                        try
+                        {
+                            string conn = para[1];
+                            ushort port = ushort.Parse(para[2]);
+                            cli1.Connect(conn, port);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        continue;
+                    }
+
+                    if (cmd.ToLower().IndexOf("send") != -1)
+                    {
+                        cmd = cmd.ToLower();
+                        string[] para = cmd.Split(' ');
+
+                        if (para.Length != 2)
+                        {
+                            Console.WriteLine("Error Command");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                cli1.Send(para[1]);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+
+                        continue;
+                    }
+                    Console.WriteLine("Unkown Command");
+
+                }//end of while
+
+                Console.WriteLine("End service");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        void ClientConn(object sender, NetEventArgs e)
+        {
+            string info = string.Format("A Client:{0} connect server :{1}", e.Client,
+             e.Client.ClientSocket.RemoteEndPoint.ToString());
+
+            Console.WriteLine(info);
+            Console.Write(">");
+        }
+
+        void ClientClose(object sender, NetEventArgs e)
+        {
+            string info;
+
+            if (e.Client.TypeOfExit == Session.ExitType.ExceptionExit)
+            {
+                info = string.Format("A Client Session:{0} Exception Closed.",
+                 e.Client.ID);
+            }
+            else
+            {
+                info = string.Format("A Client Session:{0} Normal Closed.",
+                 e.Client.ID);
+            }
+
+            Console.WriteLine(info);
+            Console.Write(">");
+        }
+
+        void RecvData(object sender, NetEventArgs e)
+        {
+            string info = string.Format("recv data:{0} from:{1}.", e.Client.Datagram, e.Client);
+            Console.WriteLine(info);
+            Console.Write(">");
+        }
+    }
+}
